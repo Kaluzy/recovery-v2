cf0efb91e29cc085fdda6d16815a3c4f63ded737
Kaluzy <88167819+Kaluzy@users.noreply.github.com>
2026-09-20 21:43:28 -0400

    Log Day 7 chicken wrap dinner

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ const seed={version:12,weight:200.8,goal:163,calGoal:1750,proGoal:120,fasting:fa
 {id:605,day:6,name:'Turkey sausage + lentil grain bowl',detail:'1 turkey sausage with leftover split peas/lentils/legumes, grain and shredded cheese',kcal:650,protein:32},
 {id:603,day:6,name:'Sweet snacks',detail:'Small pumpkin-spice muffin + 1 ice cream',kcal:350,protein:5},
 {id:604,day:6,name:'Other calories',detail:'Unlisted beverage calories',kcal:300,protein:0},
-{id:701,day:7,name:'Genfo with beef & Greek yogurt',detail:'Genfo with about 2 tbsp cooked minced beef, 1 heaping tbsp Greek yogurt, berbere and mitmita',kcal:620,protein:20}
+{id:701,day:7,name:'Genfo with beef & Greek yogurt',detail:'Genfo with about 2 tbsp cooked minced beef, 1 heaping tbsp Greek yogurt, berbere and mitmita',kcal:620,protein:20},
+{id:702,day:7,name:'Chicken wrap',detail:'Prepped chicken, lettuce/salad, cottage cheese, light mayo, hot sauce and a little shredded Mexican cheese in a large tortilla',kcal:520,protein:45}
 ]};
 const foods={tuna:{name:'Tuna crunch bowl',detail:'Tuna in water + large salad + light dressing',kcal:350,protein:35},chicken:{name:'Chicken vegetable plate',detail:'Chicken breast + vegetables + small starch portion',kcal:500,protein:45},fast:{name:'Fasting protein bowl',detail:'Tofu + lentils + vegetables + measured oil',kcal:500,protein:30},sub:{name:'Tuna sub',detail:'Multigrain bread + tuna + light mayo + cottage cheese + vegetables',kcal:575,protein:40},sardine:{name:'Sardine legume bowl',detail:'Legumes/chickpeas + sardines + vegetables',kcal:600,protein:42},cottage:{name:'Cottage cheese plate',detail:'Low-fat cottage cheese + vegetables + protein bread',kcal:380,protein:35}};
 
